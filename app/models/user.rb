cda0eeb2028cd02aaa25e250e5706c5e93f44165
SaveryIV <soriaagustin8@gmail.com>
2024-08-08 16:11:07 -0300

    Update relation of User with other tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   require "securerandom"
   has_secure_password
-  has_many :contents, foreign_key: 'author_id'
+  has_many :contents, foreign_key: 'author_id', dependent: :destroy
+  has_many :ratings, dependent: :destroy
+  has_many :rated_contents, through: :ratings, source: :content
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
   validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
 end
