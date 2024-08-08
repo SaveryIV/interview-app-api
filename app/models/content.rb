b15878a365af04ba18865e9da16c3b81df2e8ce0
SaveryIV <soriaagustin8@gmail.com>
2024-08-08 16:13:47 -0300

    Create of Contents controller, Create Content model, Update Routes

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,13 @@
 class Content < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :ratings, dependent: :destroy
+  has_many :raters, through: :ratings, source: :user
   
+  def average_rating
+    return 0 if ratings.empty?
+    ratings.average(:stars).round(2)
+  end
+
   validates :title, presence: true, uniqueness: false
   validates :description, presence: true
   validates :category, presence: true
